a6526c770b122b60c79a6c3aebbf927fd5264e5b
David Herman <bitspittle@gmail.com>
2024-02-25 17:13:36 -0800

    Add spellchecking support for common Kobweb-related words
    Fixes #31

---
+++ plugin/src/main/kotlin/com/varabyte/kobweb/intellij/spellcheck/KobwebDictionary.kt
@@ -0,0 +1,29 @@
+package com.varabyte.kobweb.intellij.spellcheck
+
+import com.intellij.spellchecker.dictionary.Dictionary
+import com.intellij.spellchecker.dictionary.RuntimeDictionaryProvider
+
+class KobwebDictionaryProvider : RuntimeDictionaryProvider {
+    override fun getDictionaries(): Array<Dictionary> = arrayOf(KobwebDictionary())
+}
+
+/**
+ * A collection of nonstandard words that are commonly encountered in Kobweb projects
+ */
+class KobwebDictionary : Dictionary {
+    private val words = setOf(
+        "frontmatter",
+        "kobweb",
+        "varabyte",
+    )
+
+    override fun getName(): String {
+        return "Kobweb Dictionary"
+    }
+
+    override fun contains(word: String): Boolean {
+        return words.contains(word.lowercase())
+    }
+
+    override fun getWords() = emptySet<String>()
+}
