dc44f7e901f56334c763b66406da87fcb0d66188
Dennis Tsar <45399002+DennisTsar@users.noreply.github.com>
2024-08-31 00:12:17 -0400

    Mark K1 code as legacy

--- plugin/src/main/kotlin/com/varabyte/kobweb/intellij/inspections/UnusedInspectionSuppressor.kt
+++ plugin/src/main/kotlin/com/varabyte/kobweb/intellij/inspections/UnusedInspectionSuppressor.kt
@@ -7,8 +7,8 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.util.CachedValue
 import com.intellij.util.containers.map2Array
 import com.varabyte.kobweb.intellij.util.kobweb.isInReadableKobwebProject
+import com.varabyte.kobweb.intellij.util.psi.hasAnyAnnotationK1
 import com.varabyte.kobweb.intellij.util.psi.hasAnyAnnotation
-import com.varabyte.kobweb.intellij.util.psi.hasAnyAnnotationK2
 import org.jetbrains.kotlin.idea.base.plugin.KotlinPluginModeProvider
 import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.psi.KtNamedFunction
@@ -16,16 +16,13 @@ import org.jetbrains.kotlin.psi.KtNamedFunction
 private val ANNOTATION_GENERATES_CODE_KEY = Key<CachedValue<Boolean>>("ANNOTATION_GENERATES_CODE")
 
 private val SUPPRESS_UNUSED_WHEN_ANNOTATED_WITH = arrayOf(
-    "com.varabyte.kobweb.api.Api",
-    "com.varabyte.kobweb.api.init.InitApi",
-    "com.varabyte.kobweb.core.App",
-    "com.varabyte.kobweb.core.Page",
-    "com.varabyte.kobweb.core.init.InitKobweb",
-    "com.varabyte.kobweb.silk.init.InitSilk",
+    ClassId.fromString("com/varabyte/kobweb/api/Api"),
+    ClassId.fromString("com/varabyte/kobweb/api/init/InitApi"),
+    ClassId.fromString("com/varabyte/kobweb/core/App"),
+    ClassId.fromString("com/varabyte/kobweb/core/Page"),
+    ClassId.fromString("com/varabyte/kobweb/core/init/InitKobweb"),
+    ClassId.fromString("com/varabyte/kobweb/silk/init/InitSilk"),
 )
-
-private val SUPPRESS_UNUSED_WHEN_ANNOTATED_WITH_IDS = SUPPRESS_UNUSED_WHEN_ANNOTATED_WITH
-    .map2Array { ClassId.fromString(it.replace('.', '/')) }
 
 /**
  * Suppress the "Unused code" inspection, when we know that Kobweb will generate code that uses it.
@@ -39,11 +36,18 @@ class UnusedInspectionSuppressor : InspectionSuppressor {
             ?: return false
 
         return if (KotlinPluginModeProvider.isK2Mode()) {
-            ktFunction.hasAnyAnnotationK2(ANNOTATION_GENERATES_CODE_KEY, *SUPPRESS_UNUSED_WHEN_ANNOTATED_WITH_IDS)
-        } else {
             ktFunction.hasAnyAnnotation(ANNOTATION_GENERATES_CODE_KEY, *SUPPRESS_UNUSED_WHEN_ANNOTATED_WITH)
+        } else {
+            ktFunction.hasAnyAnnotationK1(ANNOTATION_GENERATES_CODE_KEY, *SUPPRESS_UNUSED_WHEN_ANNOTATED_WITH_FQNS)
         }
     }
 
     override fun getSuppressActions(element: PsiElement?, toolId: String) = emptyArray<SuppressQuickFix>()
 }
+
+// region K1 legacy
+
+private val SUPPRESS_UNUSED_WHEN_ANNOTATED_WITH_FQNS = SUPPRESS_UNUSED_WHEN_ANNOTATED_WITH
+    .map2Array { it.asFqNameString() }
+
+// endregion
