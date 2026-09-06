12f45eb0a9ac526ac47286369760e5bdfa14aa7a
David Herman <bitspittle@gmail.com>
2026-09-06 11:55:27 -0700

    Fix sync error message that we're running into with latest IDE / versions

--- kobweb-model/build.gradle.kts
+++ kobweb-model/build.gradle.kts
@@ -2,7 +2,6 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlin.jvm)
-    alias(libs.plugins.intellij.base)
 }
 
 group = "com.varabyte.kobweb.intellij.model"
@@ -10,9 +9,9 @@ version = libs.versions.kobweb.ide.plugin.get()
 
 repositories {
     mavenCentral()
-    intellijPlatform {
-        defaultRepositories()
-    }
+    // JetBrains repository for gradle-tooling-extension
+    maven("https://www.jetbrains.com/intellij-repository/releases")
+    maven("https://cache-redirector.jetbrains.com/intellij-dependencies")
 }
 
 dependencies {
@@ -20,7 +19,10 @@ dependencies {
     compileOnly(gradleApi())
     // The version doesn't matter too much here; just using something recent at the time of writing this build script.
     // As long as the project compiles and the API is backwards compatible with latest IJ APIs, we should be good.
-    compileOnly("com.jetbrains.intellij.gradle:gradle-tooling-extension:233.13135.103")
+    compileOnly("com.jetbrains.intellij.gradle:gradle-tooling-extension:233.13135.103") {
+        // gradle-api already provided above, no need to pull it in twice
+        exclude(group = "org.jetbrains.intellij.deps", module = "gradle-api")
+    }
 }
 
 // These model classes will be run using the Gradle JVM, not the IntelliJ JVM. Let's set them to
